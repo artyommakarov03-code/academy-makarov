f2193bea1ee1a31c1e69715a63113cc20342572d
artyommakarov03-code <artyom.makarov.03@bk.ru>
2026-08-04 23:28:44 +0300

    Mount branding tester greeting and idea hub

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import './data/activateAdvancedContent';
 import AppV2 from './AppV2';
+import BrandRuntime from './components/BrandRuntime';
+import GlobalIdeaHub from './components/GlobalIdeaHub';
+import TesterGreeting from './components/TesterGreeting';
 import './styles.css';
+import './experience-upgrade.css';
 
-createRoot(document.getElementById('root')).render(<AppV2 />);
+createRoot(document.getElementById('root')).render(
+  <>
+    <AppV2 />
+    <BrandRuntime />
+    <TesterGreeting />
+    <GlobalIdeaHub />
+  </>
+);
